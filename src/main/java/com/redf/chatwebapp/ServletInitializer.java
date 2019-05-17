54fb3efacb4b57d88641991791ac75a02435b475
R-DF <redf4412@gmail.com>
2019-05-17 17:15:51 +0500

    Initial commit with configured server and database

---
+++ src/main/java/com/redf/chatwebapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.redf.chatwebapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ChatWebAppApplication.class);
+    }
+
+}
