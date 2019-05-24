08fffe7928934161cbbb3d94441bba03b42c4368
R-DF <redf4412@gmail.com>
2019-05-24 20:44:55 +0500

    Heroku deploy

--- src/main/java/com/redf/chatwebapp/controller/HomeController.java
+++ src/main/java/com/redf/chatwebapp/controller/HomeController.java
@@ -2,12 +2,15 @@ package com.redf.chatwebapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
 
-    public HomeController() {
+    @ResponseBody
+    String home() {
+        return "Hello World!";
     }
 }
