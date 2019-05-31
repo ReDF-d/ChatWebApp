fdd15ae066c124b677b146a1297f95318a471aef
R-DF <redf4412@gmail.com>
2019-06-01 00:08:18 +0500

    Fully working authorization and access levels

--- src/main/java/com/redf/chatwebapp/dao/utils/UserDAO.java
+++ src/main/java/com/redf/chatwebapp/dao/utils/UserDAO.java
@@ -2,16 +2,32 @@ package com.redf.chatwebapp.dao.utils;
 
 
 import com.redf.chatwebapp.dao.entities.UserEntity;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.Nullable;
 
 public interface UserDAO {
 
-    UserEntity findByUsername(String login);
+    @Nullable
+    @Contract(pure = true)
+    static UserEntity findByUsername(String login) {
+        return null;
+    }
 
-    UserEntity create(String login, String password);
+    @Nullable
+    @Contract(pure = true)
+    static UserEntity create(String login, String password, String role) {
+        return null;
+    }
 
-    void save(UserEntity user);
+    static void save(UserEntity user) {
+    }
 
-    void update(UserEntity user);
+    static void update(UserEntity user) {
+    }
 
-    void delete(UserEntity user);
+    static void delete(UserEntity user) {
+    }
+
+    static void createAndSave(String login, String password, String role) {
+    }
 }
